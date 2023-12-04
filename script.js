84bd143823bb5fb9217a81a36563192a3bb4dffb
l1zelle <144045180+l1zelle@users.noreply.github.com>
2023-12-04 00:08:30 -0500

    12/4/23 Update

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 <script type="text/javascript">
 
 var urls = new Array();
-urls[0] = "apollo.html";
-urls[1] = "achilles.html";
-urls[2] = "iphis.html";
-urls[3] = "narcissus.html";
-urls[4] = "orpheus.html";
-urls[5] = "persephone.html";
-urls[6] = "psyche.html";
-urls[7] = "pygmalion.html";
-urls[8] = "thisbe.html";
-urls[9] = "zeus.html";
+urls[0] = "https://l1zelle.github.io/ask-a-myth/persephone.html";
+urls[1] = "https://l1zelle.github.io/ask-a-myth/orpheus.html";
+urls[2] = "https://l1zelle.github.io/ask-a-myth/narcissus.html";
+urls[3] = "https://l1zelle.github.io/ask-a-myth/achilles.html";
+urls[4] = "https://l1zelle.github.io/ask-a-myth/psyche.html";
+urls[5] = "https://l1zelle.github.io/ask-a-myth/thisbe.html";
+urls[6] = "https://l1zelle.github.io/ask-a-myth/pygmalion.html";
+urls[7] = "https://l1zelle.github.io/ask-a-myth/apollo.html";
+urls[8] = "https://l1zelle.github.io/ask-a-myth/iphis.html";
+urls[9] = "https://l1zelle.github.io/ask-a-myth/zeus.html";
 
 
 
