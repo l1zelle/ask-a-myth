e554a27cb6c2e2c3f75b0827173a5ca594ea2b70
l1zelle <144045180+l1zelle@users.noreply.github.com>
2023-12-03 23:37:39 -0500

    12/3/23 Update

---
+++ script.js
@@ -0,0 +1,21 @@
+<script type="text/javascript">
+
+var urls = new Array();
+urls[0] = "apollo.html";
+urls[1] = "achilles.html";
+urls[2] = "iphis.html";
+urls[3] = "narcissus.html";
+urls[4] = "orpheus.html";
+urls[5] = "persephone.html";
+urls[6] = "psyche.html";
+urls[7] = "pygmalion.html";
+urls[8] = "thisbe.html";
+urls[9] = "zeus.html";
+
+
+
+var random = Math.floor(Math.random()*urls.length);
+
+window.location = urls[random];
+
+</script>
